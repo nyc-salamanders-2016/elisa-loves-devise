e22e5266e02465b0067a55ded76c02fe9e0c6c17
Elisa Verna <email": null>
2016-09-18 17:27:19 -0400

    generated devise controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      }
   resources :users, only: [:new, :show, :create]
   resources :posts
 
