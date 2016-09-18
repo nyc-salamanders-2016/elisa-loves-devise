99bfd5a2248e161208925b0f8c132ba510239c36
Elisa Verna <email": null>
2016-09-18 17:25:34 -0400

    deleted some users stuff i didnt need

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :users
+  devise_for :users
+  resources :users, only: [:new, :show, :create]
   resources :posts
 
-  root to: "posts#index"
+  root to: 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
